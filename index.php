d3d33e289259487cf568107555819797c58eb964
Michael C. <Michaeldev720@gmail.com>
2024-05-23 23:36:31 -0700

    remove reset.css

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PoolDuckie's Swimming Pools</title>
-    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
--- php/Seville.php
+++ php/Seville.php
@@ -8,7 +8,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Seville Pool Facility Hours</title>
-        <link rel="stylesheet" href="../css/reset.css">
         <link rel="stylesheet" href="../css/main.css">
     </head>
     <body>
