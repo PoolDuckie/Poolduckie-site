824c3ef415f275d1865d5e3a41fc68db3c0de2d7
Michael C <Michaeldev720@gmail.com>
2024-05-28 23:17:05 -0700

    update banner

--- index.php
+++ index.php
@@ -6,35 +6,91 @@
     <title>PoolDuckie's Swimming Pools</title>
     <link rel="icon" href="image/favicon.ico" type="image/icon">
     <link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    <header class="header-main">
-        <div class="header-main-logo">
-        <img src="image/logo.svg" alt="">
-        <nav class="header-main-nav">
-            <ul>
-                <li><a href="https://poolduckie.com">HOME</a></li>
-                <li><a href="https://poolduckie.com/seville">Seville Pool Hours</a></li>
-                <li><a href="https://poolduckie.com/en-MSDS">MSDS</a></li>
-                <li><a href="https://poolduckie.com/pooltest">Pool Test</a><li>
-            </ul>
-        </nav>
+    <header>
+        <div class="main-header">
+            <div class="header-logo">
+                <img src="image/logo.svg" alt="giggity" width="100">
+                <h1>Duckie Splash Crew</h1>
+            </div>
+            <div class="head-navi">
+                <nav>
+                    <ul>
+                        <li><a href="https://poolduckie.com">HOME</a></li>
+                        <li><a href="https://poolduckie.com/seville">Seville Pool Hours</a></li>
+                        <li><a href="#">Splash News</a></li>
+                        <li><a href="#">Agua Menu</a></li>
+                        <li><a href="#https://www.invitedclubs.com/clubs/seville-golf-country-club/contact-us?contactType=private-events">Host Event</a></li>
+                        <li><a href="#">Contact Pool</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
         
-        <div class="header-main-act">
-            <ul>
-                <li><a href="#">Register</a></li>
-                <li><a href="php/login.php">Login</a></li>
-            </ul>
-        </div>
     </header>
-    <main>
+    <hr/>
 
+    <main>
+		<!--Hero-->
+        <section>
+          <div class="homehero"> 
+			<img src="image/Lagoon.jpeg" alt="Splash Pool">
+			<div class="quote1">
+  			  <h2>IT'S SUMMERTIME&nbsp; <br>AT<br> SEVILLE GOLF AND COUNTRY CLUB</h2>
+			  <p>Crafting unforgettable poolside memories.</p>
+		    </div>
+            <div class="quote2">
+			  <p>Where HARDWORK meets POOLFECTION.</p>
+            </div>
+          </div>
+        </section>
+        <hr/>
+        <section>
+            <div>
+                <h2>ABOUT US:</h2>
+                <p>Our team is united by a passion for swimming and the serene beauty of Seville Golf and Country Club. We are dedicated to creating magical moments and lifelong memories in our four stunning pools. Our team consists of diligent pool technicians and vigilant lifeguards, all led by a professional swim instructor. We strive to provide a safe, clean, and fun experience for you and your family, ensuring every visit is unforgettable. Dive in and discover the perfect blend of relaxation and excitement at our exceptional pool facilities!</p>
+                <p>Stay up to date with all our upcoming pool events by checking our calendar. If you have any questions, comments, or need assistance with accommodations for an upcoming visit, please don't hesitate to reach out to us!</p>
+            </div>
+            <img src="image/poolstaff.jpeg" alt="This isnt us">
+        </section>
+        <hr/>
+        <div class="poolinfo">
+            <section>
+                <h2>Multiple Pools</h2>
+                <p>At Seville, we have a variety of pools to suit your every need. Looking to swim some laps? We have the perfect pool for that! Want to play water basketball and enjoy drinks with fellow members? We’ve got you covered! Seeking a thrill? Come check out our two giant water slides! Or perhaps you’re looking for a shallow pool to enjoy quality time with your little one—our Splash Pool is just what you need! Whatever your preference, Seville offers the ideal pool experience for everyone.</p>
+            </section>
+            <div class="fourpools">
+			  <img src="image/Lap.png" alt="Lap Pool">
+              <img src="image/4 pools.png" alt="4 pools">
+              <img src="image/Slide.jpeg" alt="Slide Pool">
+              <img src="image/Lagoon2.jpg" alt="Lagoon">
+			</div>
+        </div>
+        <hr/>
     </main>
 
     <footer>
-
+        <div>
+            <div></div><!--reviews-->
+            <div></div><!--button-->
+            <div></div><!--sitemap, invited and k swim links, contact info-->
+        </div>
     </footer>
+
+ <!--       <div>
+            <nav>
+                <ul>
+                    <li><a href="#">Contact Us.</a></li>
+                    <li><a href="https://www.invitedclubs.com/clubs/seville-golf-country-club">Seville Golf & Country Club</a></li>
+                    <li><a href="https://www.invitedclubs.com/clubs/seville-golf-country-club/amenities/dining">The Grove</a></li>
+                    <li><a href="https://poolduckie.com/en-MSDS">MSDS</a></li>
+                    <li><a href="https://poolduckie.com/pooltest">Pool Test</a></li>
+                </ul>
+            </nav>
+        </div> -->
+   
     
 </body>
 </html>
